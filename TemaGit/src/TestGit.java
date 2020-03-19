593242317c5d679a555e45715679606cd79b4c67
SEBY <SEBY@192.168.0.4>
2020-03-19 22:30:02 +0200

    mesaj titlu licenta

---
+++ TemaGit/src/TestGit.java
@@ -0,0 +1,8 @@
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git! Denumirea proiectului de licenta este: 'Aplicatie web pentru promovarea si diseminarea creatiilor literare'");
+	}
+
+}
